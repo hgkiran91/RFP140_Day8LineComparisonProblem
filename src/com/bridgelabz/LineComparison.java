0d3deb61173d4e82e2c1fac29ac5a86c66826b3b
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-29 08:40:41 +0530

    Added: UC4

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,10 +4,41 @@ import java.util.Scanner;
 
 public class LineComparison {
 
-    public static void main(String[] args) {
+    static Scanner s = new Scanner(System.in);
 
+    static void equalsMethod(){
         System.out.println("Enter x co-ordinate of first point: ");
-        Scanner s = new Scanner(System.in);
+        int x1 = s.nextInt();
+        System.out.println("Enter y co-ordinate of first point: ");
+        int y1 = s.nextInt();
+        System.out.println("Enter x co-ordinate of second point: ");
+        int x2 = s.nextInt();
+        System.out.println("Enter y co-ordinate of second point: ");
+        int y2 = s.nextInt();
+
+        System.out.println("Enter x co-ordinate of third point: ");
+        int p1 = s.nextInt();
+        System.out.println("Enter y co-ordinate of third point: ");
+        int q1 = s.nextInt();
+        System.out.println("Enter x co-ordinate of forth point: ");
+        int p2 = s.nextInt();
+        System.out.println("Enter y co-ordinate of forth point: ");
+        int q2 = s.nextInt();
+
+        Double length_of_line1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        Double length_of_line2 = Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
+
+        System.out.printf("length of 1st end points are : %.2f %n", length_of_line1);
+        System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);
+        boolean ans = length_of_line1.equals(length_of_line2);
+        if (ans == true)
+            System.out.println("Both lines are equal.");
+        else
+            System.out.println("Both lines are not equal.");
+    }
+
+    static void pointMethod(){
+        System.out.println("Enter x co-ordinate of first point: ");
         int x1 = s.nextInt();
         System.out.println("Enter y co-ordinate of first point: ");
         int y1 = s.nextInt();
@@ -38,5 +69,10 @@ public class LineComparison {
             System.out.println("Length of first end points is less than length of second end points");
         } else
             System.out.println("Both lines are equal.");
+    }
+
+    public static void main(String[] args) {
+        equalsMethod();
+        pointMethod();
     }
 }
